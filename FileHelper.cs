c6fb42f307923b85c9fb65195c85fc3cfe1b3206
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-03-15 01:08:35 +0200

    created a clear history method that deletes all history by clearing the file

--- FileHelper.cs
+++ FileHelper.cs
@@ -11,4 +11,9 @@ class FileHelper
     {
         return "(ReadHistory coming in step 3)";
     }
+
+    public void ClearHistory()
+    {
+        Console.WriteLine("ClearHistory coming in step 3");
+    }
 }
