151a2ff2a29db2b7bcdb64c7a679d3845de54a74
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-03-15 01:03:11 +0200

    create a SaveCalculation method to save a single calculation result to the file

--- FileHelper.cs
+++ FileHelper.cs
@@ -1,4 +1,9 @@
 class FileHelper
 {
     private const string FilePath = "history.txt";
+
+    public void SaveCalculation(string entry)
+    {
+        Console.WriteLine("(SaveCalculation coming in step 3)");
+    }
 }
