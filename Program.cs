be02110759c799a40c3f1bd9036e080d9df2701d
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-03-15 11:57:09 +0200

    crated the first layout of how the calculator will be displayed

--- Program.cs
+++ Program.cs
@@ -5,5 +5,14 @@ showWelcome();
 
 while (isRunning)
 {
-    
+    Console.Clear();
+    Console.WriteLine("==============================");
+    Console.WriteLine("         CALCULATOR           ");
+    Console.WriteLine("==============================");
+    Console.WriteLine("  1. New Calculation");
+    Console.WriteLine("  2. View History");
+    Console.WriteLine("  3. Clear History");
+    Console.WriteLine("  0. Exit");
+    Console.WriteLine("------------------------------");
+    Console.Write("  Choose an option: ");   
 }
